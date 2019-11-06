b8d8c5cb09fcc2575c7538e31c77466c8ac51081
Patrick McDonagh <patrickjmcd@gmail.com>
2019-11-05 20:55:22 -0600

    build new version

---
+++ docs/precache-manifest.2d31665b25c4edc41b53c7d627383509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4826fff91810805ad9ff584b520e2e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a1e7f35fc4c6d05ba8",
+    "url": "/static/css/main.2585e7b9.chunk.css"
+  },
+  {
+    "revision": "4b191f0abe971622c2b6",
+    "url": "/static/js/2.ae72974d.chunk.js"
+  },
+  {
+    "revision": "e8a1e7f35fc4c6d05ba8",
+    "url": "/static/js/main.73e1710f.chunk.js"
+  },
+  {
+    "revision": "a5594ad7b3d30cd561f3",
+    "url": "/static/js/runtime-main.c15c086a.js"
+  },
+  {
+    "revision": "6406617f904e216efad98c7a67ce07b3",
+    "url": "/static/media/coffee-beans.6406617f.jpg"
+  }
+]);
